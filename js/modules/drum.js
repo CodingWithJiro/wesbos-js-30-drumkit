d4625c75229af2c2199b9b9920b66d6b3f4d283b
CodingWithJiro <chavezelmar03@gmail.com>
2025-09-03 13:58:27 +0800

    feat: create const variables for buttons and audio elements

--- js/modules/drum.js
+++ js/modules/drum.js
@@ -1,3 +1,7 @@
 //* DRUM.JS SCRIPT
+const buttons = document.querySelectorAll(".drum__button");
+const sounds = document.querySelectorAll(".drum__audio");
+
+console.log(sounds);
 
 export function initDrum() {}
